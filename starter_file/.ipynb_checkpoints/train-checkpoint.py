97ecd7d83bfdf74c842bff0a84336a91fd368383
Panth-Shah <panth5shah@gmail.com>
2021-02-02 05:34:16 +0000

    [Capstone Project] - AutoML Run details added

--- starter_file/.ipynb_checkpoints/train-checkpoint.py
+++ starter_file/.ipynb_checkpoints/train-checkpoint.py
@@ -3,7 +3,6 @@ import argparse
 import os
 import numpy as np
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
